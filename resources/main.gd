2faf6f534bacf4c9a4b050ef74ac39defd5a2d1f
Luke Picciau <luke.b.picciau@gmail.com>
2014-08-03 16:36:54 +0930

    Removed broken user inputs

--- resources/main.gd
+++ resources/main.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 var moves         = []
 var cubes         = ["Red","Blue","Green","Yellow"]
-var user_turn     = false
-var user_move     = []
-var user_turns    = 0
 var game_running  = false
 var display_move  = 0
 var pressed
@@ -14,6 +11,7 @@ var enter_pressed
 
 func _ready():
 	set_process(true)
+
 
 func _process(delta):
 	start()
@@ -31,45 +29,13 @@ func _process(delta):
 			enter_pressed = true
 	else:
 		enter_pressed = false
-	
-	if user_turns == moves.size():
-		user_turn = false
-	
-	
-	if user_turn: #Gets user input after animation plays
-		if Input.is_action_pressed("red"):
-			user_move.push_back("Red")
-			user_turns += 1
-			print (user_move)
-		if Input.is_action_pressed("blue"):
-			user_move.push_back("Blue")
-			user_turns += 1
-			print (user_move)
-		if Input.is_action_pressed("green"):
-			user_move.push_back("Green")
-			user_turns += 1
-			print (user_move)
-		if Input.is_action_pressed("yellow"):
-			user_move.push_back("Yellow")
-			user_turns += 1
-			print (user_move)
+
 
 func start():  #Plays animations to show the patern
 	if game_running:
 		print ("Game started")
-		var i = 0
-		
 		get_node("Timer").start()
-
-		user_turn = true
 		game_running = false
-	
-	
-
-func turn_end():
-	if moves == user_turns:
-		new_move()
-		start()
 
 
 func new_move(): #adds a new move to the pattern
