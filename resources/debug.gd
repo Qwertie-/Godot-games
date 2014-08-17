adbbcd1ecd57640777dc85421e046cff6bf1fb35
Luke Picciau <luke.b.picciau@gmail.com>
2014-08-17 21:18:01 +0930

    Fixed typo in debug script

--- resources/debug.gd
+++ resources/debug.gd
@@ -7,6 +7,6 @@ func _ready():
 	
 func _process(delta):
 	get_node("users_turn").set_text(str(get_parent().users_turn))
-	get_node("game_running").set_text(str(get_parent().users_turn))
+	get_node("game_running").set_text(str(get_parent().game_running))
 
 
