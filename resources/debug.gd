6af4973292c7cbf44355f24958d0774c8fabe46a
Luke Picciau <luke.b.picciau@gmail.com>
2014-08-17 18:24:47 +0930

    Removed bad naming in debug.gd

--- resources/debug.gd
+++ resources/debug.gd
@@ -6,6 +6,5 @@ func _ready():
 	set_process(true)
 	
 func _process(delta):
-	var debug1 = get_parent().users_turn
-	get_node("users_turn").set_text(str(debug1))
+	get_node("users_turn").set_text(str(get_parent().users_turn))
 
