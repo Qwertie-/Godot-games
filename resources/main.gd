dcc8075f2710a045735b410c2a95c4a09c827c01
Luke Picciau <luke.b.picciau@gmail.com>
2014-08-24 17:43:52 +0930

    Working on getting user turns added

--- resources/main.gd
+++ resources/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 var moves         = []
 var cubes         = ["Red","Blue","Green","Yellow"]
 var game_running  = false
-var display_move  = 0
+var display_move  = -1
 var start_pressed = false
 var users_turn    = true
 var user_move     = []
@@ -89,6 +89,9 @@ func _on_Timer_timeout():
 			print ("Move " + str(display_move) + " " + "is Yellow")
 			get_node("AnimationPlayer").queue("Blink_yellow")
 		display_move += 1
+	elif display_move == moves.size():
+		display_move += 1
+		
 	if display_move == moves.size() && !users_turn:
 		print ("start user turn")
 		users_turn = true
