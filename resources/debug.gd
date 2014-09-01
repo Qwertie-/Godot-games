f2791c03ab9d2e98e413883e95c632b629d2d9d6
Luke Picciau <luke.b.picciau@gmail.com>
2014-09-01 17:53:30 +0930

    Upgraded the debug script

--- resources/debug.gd
+++ resources/debug.gd
@@ -11,17 +11,22 @@ func _process(delta):
 	if Input.is_action_pressed("debug"):
 		if !debug_toggle_pressed:
 			if !debug:
+				show()
 				debug = true
+			else:
+				hide()
+				debug = false
+			debug_toggle_pressed = true
+			
+	else:
+		debug_toggle_pressed = false
 
-
-	
-	if debug:
-		get_node("users_turn").set_text(str(get_parent().users_turn))
-		get_node("game_running").set_text(str(get_parent().game_running))
-		get_node("display_move").set_text(str(get_parent().display_move))
-		get_node("user_move").set_text(str(get_parent().user_move))
-		get_node("moves").set_text(str(get_parent().moves))
-		get_node("move_checked").set_text(str(get_parent().move_checked))
+	get_node("users_turn").set_text(str(get_parent().users_turn))
+	get_node("game_running").set_text(str(get_parent().game_running))
+	get_node("display_move").set_text(str(get_parent().display_move))
+	get_node("user_move").set_text(str(get_parent().user_move))
+	get_node("moves").set_text(str(get_parent().moves))
+	get_node("move_checked").set_text(str(get_parent().move_checked))
 
 
 
